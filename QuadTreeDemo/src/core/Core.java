90f2851296b49ad9966b75a566af391b0482851d
Apollon <Apollon@Apollon-PC>
2014-12-09 13:22:02 +0200

    Implemented collisions. Added "R" to clear, "E" to insert random, right mouse click to draw rect.

--- QuadTreeDemo/src/core/Core.java
+++ QuadTreeDemo/src/core/Core.java
@@ -18,9 +18,12 @@ public class Core extends PApplet {
 	private long t;
 	private QuadTree quadTree;
 	private ArrayList<Entity> objects;
-	private int objectsNum = 1000;
+	private ArrayList<Entity> tempObjects;
+	private int objectsNum = 500;
 	private Random rng;
 	private boolean shouldColor;
+	private boolean drawingUserRect;
+	private double userX, userY, userX2, userY2;
 
 	public void setup() {
 		instance = this;
@@ -28,11 +31,12 @@ public class Core extends PApplet {
 		t = millis();
 		frame.setTitle("QuadTreeDemo");
 		rng = new Random();
-		quadTree = new QuadTree(0, new Rectangle(0, 0, width, height), 10, 5);
+		quadTree = new QuadTree(0, new Rectangle(0, 0, width, height), 5, 10);
 		objects = new ArrayList<Entity>();
+		tempObjects = new ArrayList<Entity>();
 		for (int i = 0; i < objectsNum; i++) {
-			int w = rng.nextInt(width / 16);
-			int h = rng.nextInt(height / 16);
+			int w = rng.nextInt(width / 32);
+			int h = rng.nextInt(height / 32);
 			int x = rng.nextInt(width - w);
 			int y = rng.nextInt(height - h);
 			Rectangle r = new Rectangle(x, y, w, h);
@@ -46,10 +50,26 @@ public class Core extends PApplet {
 	}
 
 	public void input() {
-		if (mousePressed) {
+		if (mousePressed && mouseButton == 37) {
 			shouldColor = true;
 		} else {
 			shouldColor = false;
+		}
+		if (mousePressed && mouseButton == 39 && !drawingUserRect) {
+			userX = mouseX;
+			userY = mouseY;
+			drawingUserRect = true;
+		}
+		if (!mousePressed && drawingUserRect) {
+			userX2 = mouseX;
+			userY2 = mouseY;
+			drawingUserRect = false;
+
+			objectsNum++;
+
+			objects.add(new Rectangle(Math.min(userX, userX2), Math.min(userY,
+					userY2), Math.max(userX, userX2) - Math.min(userX, userX2),
+					Math.max(userY, userY2) - Math.min(userY, userY2)));
 		}
 	}
 
@@ -66,14 +86,33 @@ public class Core extends PApplet {
 			objects.get(i).setColor(100);
 		}
 		frame.setTitle("QuadTreeDemo");
+		tempObjects.clear();
+
+		quadTree.clear();
+		for (Entity e : objects) {
+			quadTree.insert(e);
+		}
+
 		if (shouldColor) {
-			List<Entity> entities = quadTree.retrieve(mouseX, mouseY);
+			List<Entity> entities = quadTree.retrieve(tempObjects, mouseX,
+					mouseY);
 			for (Entity e : entities) {
 				e.setColor(250);
 			}
 			frame.setTitle("QuadTreeDemo: Need to check collision with only "
 					+ entities.size() + "/" + (objectsNum) + " objects.");
+		} else {
+			tempObjects.clear();
+			for (Entity e : objects) {
+				for (Entity e2 : quadTree.retrieve(tempObjects, e)) {
+					if (e != e2 && e.collidesWith(e2)) {
+						e.setColor(250);
+						e2.setColor(250);
+					}
+				}
+			}
 		}
+
 	}
 
 	public void draw() {
@@ -102,6 +141,14 @@ public class Core extends PApplet {
 		}
 
 		drawQuadTree(quadTree);
+		if (drawingUserRect) {
+			noStroke();
+			fill(160, 100, 150, 50);
+			rect((float) Math.min(userX, mouseX),
+					(float) Math.min(userY, mouseY),
+					(float) (Math.max(userX, mouseX) - Math.min(userX, mouseX)),
+					(float) (Math.max(userY, mouseY) - Math.min(userY, mouseY)));
+		}
 		noStroke();
 
 	}
@@ -121,13 +168,32 @@ public class Core extends PApplet {
 	}
 
 	public void mousePressed() {
-
+		println(mouseButton);
 	}
 
 	public void mouseReleased() {
 	}
 
 	public void keyPressed() {
+		println(keyCode);
+
+		// R
+		if (keyCode == 82) {
+			objects.clear();
+			objectsNum = 0;
+		}
+
+		// E
+		if (keyCode == 69) {
+			int w = rng.nextInt(width / 32);
+			int h = rng.nextInt(height / 32);
+			int x = rng.nextInt(width - w);
+			int y = rng.nextInt(height - h);
+			Rectangle r = new Rectangle(x, y, w, h);
+			r.setColor(100);
+			objects.add(r);
+			objectsNum++;
+		}
 	}
 
 	public void keyReleased() {
